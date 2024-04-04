3c761e379ceb6bfa42d4e1a73fa5b01a3e803ee5
David Wogelius <davidwogelius@gmail.com>
2024-04-04 20:42:52 +0200

    japan v0.0.4
    design done

--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,9 +1,8 @@
 <template>
     <TopNav/>
     <div>
-        <img src="" alt="">
         <div class="titleContainer">
-        <h2>Japan</h2>
+            <h2>Japan</h2>
         </div>
         <p>
             Japan byder på en mangfoldig plantenatur, der spænder fra de skovklædte bjerge i nord til de frodige regnskove i syd. I nord findes nåletræer som gran og fyr, mens de subtropiske områder i syd er hjemsted for eksotiske træer, buske og blomster. Bambus er udbredt, og sakura (kirsebærblomster) er en ikonisk del af japansk kultur. Den rige plantenatur afspejler Japans kulturelle arv og forbindelse til naturen. Japan's planteliv udgør også et fundament for landets økologiske balance og støtter en rig biodiversitet af både flora og fauna. Denne dybe forbindelse til naturen er en central del af japansk identitet og tradition, og den afspejler sig i alt fra kunst og litteratur til dagliglivet.
@@ -32,14 +31,31 @@
     .buttonContainer {
         display: flex;
         flex-direction: column;
+        gap: 30px;
     }
     p{
         color: black;
+        text-align:center;
+        padding: 10px 20px 10px 20px;
+
     }
     .titleContainer{
         background-image: url('../assets/images/Yucca-flaccida-palmelilje-1024x683.png');
         text-align: center;
-        padding: 20px;
+        height: 179px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        h2{
+            color: white;
+        }
+        
+
+        
+    }
+    a{
+        margin-left: auto;
+        margin-right: auto;
     }
 
 </style>
