10f9d8bfa202f6d3dac4dbaf4619fa03983b2ee1
David Wogelius <davidwogelius@gmail.com>
2024-04-04 20:18:26 +0200

    japan v0.0.3

--- src/router/index.js
+++ src/router/index.js
@@ -28,13 +28,16 @@ const router = createRouter({
       component: () => import('../views/CalenderView.vue')
     },
     {
+      path: '/404',
+      name: '404',
+      component: () => import('../views/404View.vue')
+    },
+    {
       path: '/country',
       name: 'country',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/CountryView.vue')
     },
+
   ]
 })
 
