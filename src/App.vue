fe11a9f716f8c7622694fb6efc55808a0366a61a
David Wogelius <davidwogelius@gmail.com>
2024-04-07 15:38:39 +0200

    loading spinner v0.0.1
    loading spinner til når appen loader.
    
    note: er pt. sat til at simulere en loading på 2000ms, skal refaktoreres til en async/await når backenden/API/server skal opsættes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import bottomNav from './components/bottomNav.vue';
-</script>
-
 <template>
-      <bottomNav/>
-  <RouterView />
+  <div>
+    <!-- Use conditional rendering to display either the loading spinner or router-view -->
+    <template v-if="isLoading">
+      <loading-spinner :loading="true" />
+    </template>
+    <template v-else>
+      <router-view />
+    </template>
+    <!-- Always display BottomNav -->
+    <BottomNav />
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref, onMounted } from 'vue';
+import LoadingSpinner from './components/LoadingSpinner.vue';
+import BottomNav from './components/bottomNav.vue';
 
+const isLoading = ref(false);
+
+const fetchData = () => {
+  // Simulating app loading
+  isLoading.value = true;
+  setTimeout(() => {
+    // Your API logic should be here
+    // When API loading is finished, set isLoading back to false
+    isLoading.value = false;
+  }, 2000);
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+<style scoped lang="scss">
+/* Your scoped styles here */
 </style>
