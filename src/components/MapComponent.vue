7867f36c2d8f9f5aa70d1e1acc9bb207c3f2e9d9
Abd Alrhman T Alshaar Dit Darra <abdalrhmanaldarra@gmail.com>
2024-04-25 12:44:40 +0200

    Map created

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,21 +1,41 @@
 <template>
-    <div>
-        <p>Dette er placeholder for the-soon-to-be google maps api</p>
-    </div>
-</template>
-<script setup>
-    console.log('mapview')
-</script>
-<style lang="scss" scoped>
-div{
-    border-style: dashed;
-    justify-content: center;
-    height: 480px;
-    p{
-        padding: 70px 0 0 0;
-        height: 100% ;
-        text-align: center;
+    <div ref="mapContainer" class="map-container"></div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted, watch } from 'vue';
+  import L from 'leaflet';
+  import 'leaflet/dist/leaflet.css';
+  
+  const props = defineProps({
+    selectedArea: Object
+  });
+  
+  const mapContainer = ref(null);
+  let map = null;
+  let marker = null;
+  
+  onMounted(() => {
+    map = L.map(mapContainer.value).setView([55.6761, 12.5683], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+  });
+  
+  watch(() => props.selectedArea, (newValue) => {
+    if (newValue) {
+      if (marker) map.removeLayer(marker);
+      marker = L.marker([newValue.lat, newValue.lng], { draggable: true })
+        .addTo(map)
+        .bindPopup(newValue.name);
+      map.setView([newValue.lat, newValue.lng], 13);
     }
-}
-    
-</style>
+  });
+  </script>
+  
+  <style scoped>
+  .map-container {
+    height: 400px;
+  }
+  </style>
+  
