e41159feeddc3d2194230b34bb6db3efcde9c05e
Nikolai Jensen <nikolaihjensen@hotmail.com>
2024-05-07 12:59:11 +0200

    import of pinia in testtwo

--- src/components/__tests__/LoginView.test.js
+++ src/components/__tests__/LoginView.test.js
@@ -1,24 +1,28 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import LoginView from '@/views/LoginView.vue'
 import { test as it } from 'vitest'
+import { renderToString } from '@vue/server-renderer'
 
-it('renders a form', () => {
+it('renders a form', async () => {
   const div = document.createElement('div')
   document.body.appendChild(div)
 
-  const pinia = createPinia();
-  createApp(LoginView).mount(div)
+  const pinia = createPinia()
+  const app = createApp(LoginView).use(pinia)
+  div.innerHTML = await renderToString(app)
 
   expect(document.querySelector('form')).not.toBeNull()
 })
 
-it('renders email and password input fields', () => {
+it('renders email and password input fields', async () => {
   const div = document.createElement('div')
   document.body.appendChild(div)
 
-  const pinia = createPinia();
-  createApp(LoginView).mount(div)
+  const pinia = createPinia()
+  const app = createApp(LoginView).use(pinia)
+  div.innerHTML = await renderToString(app)
 
   expect(document.querySelector('input[type="email"]')).not.toBeNull()
   expect(document.querySelector('input[type="password"]')).not.toBeNull()
