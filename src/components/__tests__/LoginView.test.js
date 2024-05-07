1ba9e15472f8d63d6c7120656451e13574667802
Nikolai Jensen <nikolaihjensen@hotmail.com>
2024-05-07 12:56:24 +0200

    import of pinia in test

--- src/components/__tests__/LoginView.test.js
+++ src/components/__tests__/LoginView.test.js
@@ -1,11 +1,13 @@
-import { test as it } from 'vitest'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import LoginView from '@/views/LoginView.vue'
+import { test as it } from 'vitest'
 
 it('renders a form', () => {
   const div = document.createElement('div')
   document.body.appendChild(div)
 
+  const pinia = createPinia();
   createApp(LoginView).mount(div)
 
   expect(document.querySelector('form')).not.toBeNull()
@@ -15,6 +17,7 @@ it('renders email and password input fields', () => {
   const div = document.createElement('div')
   document.body.appendChild(div)
 
+  const pinia = createPinia();
   createApp(LoginView).mount(div)
 
   expect(document.querySelector('input[type="email"]')).not.toBeNull()
