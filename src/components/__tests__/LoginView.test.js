3950af04aa828c250b3ab905f2a32dbba8d54f2f
Nikolai Jensen <nikolaihjensen@hotmail.com>
2024-05-07 13:02:31 +0200

    import of pinia in testtwo

--- src/components/__tests__/LoginView.test.js
+++ src/components/__tests__/LoginView.test.js
@@ -1,9 +1,9 @@
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import LoginView from '@/views/LoginView.vue'
 import { test as it } from 'vitest'
-import { renderToString, expect } from '@vue/server-renderer'
+import { renderToString } from '@vue/server-renderer'
+import assert from 'assert'
 
 it('renders a form', async () => {
   const div = document.createElement('div')
@@ -13,7 +13,7 @@ it('renders a form', async () => {
   const app = createApp(LoginView).use(pinia)
   div.innerHTML = await renderToString(app)
 
-  expect(document.querySelector('form')).not.toBeNull()
+  assert(document.querySelector('form') !== null, 'Form is not rendered')
 })
 
 it('renders email and password input fields', async () => {
@@ -24,6 +24,6 @@ it('renders email and password input fields', async () => {
   const app = createApp(LoginView).use(pinia)
   div.innerHTML = await renderToString(app)
 
-  expect(document.querySelector('input[type="email"]')).not.toBeNull()
-  expect(document.querySelector('input[type="password"]')).not.toBeNull()
+  assert(document.querySelector('input[type="email"]') !== null, 'Email input field is not rendered')
+  assert(document.querySelector('input[type="password"]') !== null, 'Password input field is not rendered')
 })
