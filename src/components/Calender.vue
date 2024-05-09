38f5dfbb064c4eab38abebcccdcde0110bcfbfab
Abd Alrhman T Alshaar Dit Darra <abdalrhmanaldarra@gmail.com>
2024-05-09 12:41:52 +0200

    calandar highlight events  dates

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
-    
-    <VDatePicker v-model="date" />
+    <VDatePicker 
+      v-model="date"
+      :attributes="attrs"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { Calendar as VCalendar, DatePicker as VDatePicker } from 'v-calendar';
+import { ref, computed } from 'vue';
+import { DatePicker as VDatePicker } from 'v-calendar';
+
+const props = defineProps({
+  events: Array 
+});
 
 const date = ref(new Date());
+
+const attrs = computed(() => {
+  return props.events.map(event => ({
+    key: event.date,
+    highlight: {
+      class: 'event-highlight',
+    },
+    dates: event.date, 
+  }));
+});
+
 </script>
 
 <style scoped>
-
+.event {
+  background-color: #ffecb3; 
+}
 </style>
