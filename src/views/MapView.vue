93f013744cf05a98151d9f4b1a8c96ba0423eef3
David Wogelius <davidwogelius@gmail.com>
2024-05-16 12:27:44 +0200

    Squashed commit of the following:
    commit 41da73fa30f4cb03214be91dec505f8ef632ce17
    Author: Abd Alrhman T Alshaar Dit Darra <abdalrhmanaldarra@gmail.com>
    Date:   Thu May 16 12:02:14 2024 +0200
    
        update of coords
    
        we have now lat and lng in the database so we dont have them static in the location.js file
    
    commit 32369f302271a5dca88be5384832dc96f535250d
    Merge: ef6146c 353299b
    Author: Nikolai Høj Jensen <93127755+Nikolaihoej@users.noreply.github.com>
    Date:   Thu May 16 09:58:40 2024 +0200
    
        Merge pull request #29 from WEB-Team5-2024/audio2
    
        audio component
    
    commit 353299b6006d9e361f8db93833cb4c037d0972de
    Author: David Wogelius <davidwogelius@gmail.com>
    Date:   Wed May 15 21:54:58 2024 +0200
    
        audio component
    
        audiocomponent der får audioUrl fra mapview

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -7,6 +7,9 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import { useLocationStore } from '@/stores/location';
+
 import TopNav from '../components/TopNav.vue';
 import MapComponent from '../components/MapComponent.vue';
 import AreaCard from '../components/AreaCard.vue';
@@ -19,149 +22,35 @@ function handleDistanceCalculation(distance) {
     distanceResult.value = distance;
 }
 
-</script>
 
+const locationStore = useLocationStore();
+
+onMounted(() => {
+  locationStore.fetchGardenAreas();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      locationStore.updateCurrentPosition({ lat: latitude, lng: longitude });
+    });
+  }
+});
+</script>
+const locationStore = useLocationStore();
+
+onMounted(() => {
+  locationStore.fetchGardenAreas();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      locationStore.updateCurrentPosition({ lat: latitude, lng: longitude });
+    });
+  }
+});
+</script>
 
 <style scoped lang="scss">
 @import '@/styles/global.scss';
 @import 'bootstrap-icons/font/bootstrap-icons.css';
-
-.menuContainer {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    margin: 8px 15px 0px 15px;
-    margin-bottom: 100px;
-
-    .menuItem {
-        display: flex;
-        align-items: center;
-        background: $secondary-color;
-        box-shadow: $menuContainer-shadow;
-        border-radius: $border-radius;
-        padding: 20px;
-        color: $font-color;
-        position: relative;
-        gap: 10px;
-
-        .imageContainer {
-            flex-shrink: 0;
-            width: 100px; 
-            height: 100px; 
-            border-radius: $border-radius;
-            overflow: hidden;
-            margin-right: 10px;
-
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
-        }
-
-        .textContainer {
-            flex-grow: 1;
-
-            .titleAndDistance {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                
-                h3 {
-                    font-size: $medium-font-size;
-                    margin: 0;
-                    color: $font-color; 
-                    font-size: 16px; 
-                }
-
-                .distance {
-                    background: #FFFFFF;
-                    color: #000000;
-                    padding: 5px 10px;
-                    border-radius: 15px;
-                    font-size: 12px;
-                }
-
-                .distanceText {
-                    color: $distancetext-color;
-                }
-            }
-
-            p {
-                font-size: $small-font-size;
-                margin: 0;
-                font-size: 12px; 
-                color: $font-color;
-            }
-        }
-
-        .bi-arrow-right-circle {
-            font-size: 30px; 
-            margin-left: auto; 
-            color: #FFF; 
-        }
-    }
-}
-.selectedAreaContainer {
-    display: flex;
-    gap: 15px;
-    margin: 10px;
-  
-
-  .menuItem {
-    display: flex;
-    align-items: center;
-    background: $secondary-color;
-    box-shadow: $drop-shadow-light;
-    border-radius: $border-radius;
-    padding: 20px;
-    color: $font-color;
-    position: relative;
-
-    .imageContainer {
-            padding: 5px;
-            flex-shrink: 0;
-            width: 100px; 
-            height: 100px; 
-            border-radius: $border-radius;
-            overflow: hidden;
-            margin-right: 10px;
-
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
-    }
-
-    .textContainer {
-      h3 {
-        margin: 0;
-        color: $font-color;
-        font-size: $medium-font-size;
-      }
-
-      p {
-        margin: 0;
-        font-size: $small-font-size; 
-        color: $font-color; 
-      }
-
-      button {
-        margin-top: 20px;
-        display: flex;
-        padding: 10px 15px; 
-        border-radius: 15px; 
-        font-size: 1rem; 
-        background-color: #4A4C63; 
-        color: white;
-        border: none; 
-        cursor: pointer; 
-        display: inline-block; 
-        text-align: center; 
-      }
-    }
-  }
-}
 </style>
-
