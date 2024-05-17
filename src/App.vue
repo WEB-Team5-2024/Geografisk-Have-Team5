c3421eedf434f1cbc2c5cf53641fc5b4371564b9
David Wogelius <davidwogelius@gmail.com>
2024-05-17 14:30:32 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: AIzaSyDrmtxQRs_dtjJ_aVAWEIeZQNqx9oDhQ2A,
-  authDomain: geografisk-have-webteam5.firebaseapp.com ,
-  projectId: geografisk-have-webteam5,
-  storageBucket: geografisk-have-webteam5.appspot.com,
+  apiKey: "AIzaSyDrmtxQRs_dtjJ_aVAWEIeZQNqx9oDhQ2A",
+  authDomain: "geografisk-have-webteam5.firebaseapp.com" ,
+  projectId: "geografisk-have-webteam5",
+  storageBucket: "geografisk-have-webteam5.appspot.com",
   messagingSenderId: "350894266794",
   appId: "1:350894266794:web:8c3d3487b823536f8873d3",
-  measurementId: G-VDFEE8D343
+  measurementId: "G-VDFEE8D343"
 };
 
 console.log(import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID)
